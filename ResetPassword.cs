49e7d1c3b8189b297914f595cd6687ce0267c4ff
IAmmarIftikhar <110023334+IAMMARKHAN@users.noreply.github.com>
2023-09-20 19:14:29 +0500

    Add Password Recovery Feature

---
+++ ResetPassword.cs
@@ -0,0 +1,56 @@
+﻿using db2021finalprojectg_9;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Restuarant_App
+{
+    public partial class ResetPassword : Form
+    {
+        string email;
+        public ResetPassword(string email)
+        {
+            InitializeComponent();
+            this.email = email;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            int code=2;
+            RecoverPassword recoverPassword = new RecoverPassword(code,email);
+            recoverPassword.Show();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); 
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(textBox2.Text !="")
+            {
+                string updateCommandText = "UPDATE dbo.[user] SET password = @NewPassword WHERE email = @Email";
+                var con = Configuration.getInstance().getConnection();
+                SqlCommand command = new SqlCommand(updateCommandText, con);
+                command.Parameters.AddWithValue("@Email", email); // replace with the name of your username input textbox
+                command.Parameters.AddWithValue("@NewPassword", textBox2.Text); // replace with the name of your password input textbox
+                int count = command.ExecuteNonQuery();
+                if (count > 0)
+                {
+                    MessageBox.Show("Password Updated Successfullly!", "Success", MessageBoxButtons.OK);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please Enter New Password!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+    }
+}
